5b27d3a1949875701dd6676a996b5a172619307a
tingwei xie <tix980@mail.usask.ca>
2021-08-16 19:10:09 -0400

    update qa page

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 import './style.scss'
+
+//Tried to run mocha, but keep getting "Rollup failed to resolve import "asset" from "main.js".
+//This is most likely unintended because it can break your application at runtime." error messages.
+// import {equal} from 'asset';
+
+// const array = ["qualityAssurance/quality-assurance.html","security/security.html","badExample/bad-example.html"];
+
+// describe('myArray', () => {
+//   it('should not contain the fourth URL', () => {
+//     equal(myArray.indexOf(4), "");
+//   });
+// });
+
+//See if the Javascript has linked to the index file.
 console.log("connected!");
+
+//Displays the hyperlinks of other pages.
 document.querySelector('#app').innerHTML = `
   <h1>This is the index page!</h1>
   <p>Please select the links below!</p>
